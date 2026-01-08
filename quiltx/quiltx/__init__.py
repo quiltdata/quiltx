28cc60326978e4c8a32131e17f27749f76d65138
Dr. Ernie Prabhakar <ernest@quilt.bio>
2026-01-07 17:02:39 -0800

    Align configured_catalog with quilt3.config signature

--- quiltx/quiltx/__init__.py
+++ quiltx/quiltx/__init__.py
@@ -8,9 +8,9 @@ __all__ = ["__version__", "configured_catalog"]
 __version__ = "0.1.0"
 
 
-def configured_catalog(*args: Any, **kwargs: Any):
-    """Return a configured quilt3 Catalog."""
+def configured_catalog(*catalog_url: str, **config_values: Any):
+    """Configure quilt3 and return a Catalog."""
     import quilt3
 
-    quilt3.config(*args, **kwargs)
+    quilt3.config(*catalog_url, **config_values)
     return quilt3.Catalog()
